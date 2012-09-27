099c81b2f41bd5a544dac2da57d6870f806f8228
cbernat <cbernat@makingsense.com>
2012-09-27 16:17:58 -0300

    Add text

--- Demo/Demo/Controllers/HomeController.cs
+++ Demo/Demo/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Demo.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Git!!!!!!";
 
             return View();
         }
